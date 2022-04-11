2b5f5e80597f90a1c486d9f2d5cdff5ae4880e4d
MikhailSustretov <eugene.danceboy@gmail.com>
2022-04-11 13:34:29 +0300

    Fixed a bug due to which files were not deleted

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -6,6 +6,7 @@ use App\Models\Image;
 use App\Repositories\ImageRepository;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 use Mockery\Exception;
 
 class ImageService
@@ -26,15 +27,15 @@ class ImageService
         try {
             Db::beginTransaction();
 
-            dd(File::delete($image->title));
+            Storage::delete($image->title);
 
-            $deletedPerson = $this->imageRepository->delete($image->id);
+            $deletedImage = $this->imageRepository->delete($image->id);
             Db::commit();
 
         } catch (Exception $exception) {
             Db::rollBack();
             return $exception->getMessage();
         }
-        return $deletedPerson;
+        return $deletedImage;
     }
 }
